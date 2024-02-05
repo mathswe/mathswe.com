1fa9ab3fd4e897f2c1fdeb1940fbbe46e6d24e27
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-02-05 17:00:21 -0600

    Add some article styles

--- src/legal/Legal.tsx
+++ src/legal/Legal.tsx
@@ -3,50 +3,56 @@
 
 function Legal() {
     return <>
-        <main>
-            <article>
-                <section>
-                    <h1>Legal</h1>
-                </section>
+        <section className="main">
+            <main>
+                <article>
+                    <section>
+                        <h1>Legal</h1>
+                    </section>
 
-                <section>
-                    <h2>Cookies</h2>
-
-                    <p>
-                        In optimizing product experience, we utilize cookies for
-                        specific functionalities. Cookies are compact text files
-                        stored on user devices.
-                    </p>
-
-                    <blockquote>
-                        <p>
-                            Cookies are small pieces of text sent to your
-                            browser by a website you visit. They help that
-                            website remember information about your visit, which
-                            can both make it easier to visit the site again and
-                            make the site more useful to you.
-                        </p>
+                    <section>
+                        <h2>Cookies</h2>
 
                         <p>
-                            Source: How Google Uses Cookies | Google
+                            In optimizing product experience, we utilize cookies
+                            for
+                            specific functionalities. Cookies are compact text
+                            files
+                            stored on user devices.
                         </p>
-                    </blockquote>
 
-                    <p>
-                        You can read the Google cookie policies page to&nbsp;
-                        <a
-                            href="https://policies.google.com/technologies/cookies"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            learn about cookies
-                        </a>
-                        , and their usage.
-                    </p>
-                </section>
-            </article>
-        </main>
+                        <blockquote>
+                            <p>
+                                Cookies are small pieces of text sent to your
+                                browser by a website you visit. They help that
+                                website remember information about your visit,
+                                which
+                                can both make it easier to visit the site again
+                                and
+                                make the site more useful to you.
+                            </p>
 
+                            <p>
+                                Source: How Google Uses Cookies | Google
+                            </p>
+                        </blockquote>
+
+                        <p>
+                            You can read the Google cookie policies page
+                            to&nbsp;
+                            <a
+                                href="https://policies.google.com/technologies/cookies"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                learn about cookies
+                            </a>
+                            , and their usage.
+                        </p>
+                    </section>
+                </article>
+            </main>
+        </section>
     </>;
 }
 
