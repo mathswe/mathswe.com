73a9ec834cf503fe696e9af9812debf4e259837d
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-02-05 15:06:21 -0600

    Add base project config from current MSW-MVP app

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+// @ts-expect-error This dev dep must be fixed to support its types correctly
+import eslintPlugin from "vite-plugin-eslint";
+import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-                                plugins: [ react() ],
-                            });
+export default defineConfig(
+    {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+        plugins: [ react(), eslintPlugin() ],
+        resolve: {
+            alias: {
+                "@": resolve(__dirname, "src"),
+            },
+        },
+    },
+);
