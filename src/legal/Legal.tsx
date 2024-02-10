92778a9bdbeefa40067c1659dd41c16d6cb3fcbb
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-02-10 15:18:59 -0600

    Extract component Cookies from Legal

--- src/legal/Legal.tsx
+++ src/legal/Legal.tsx
@@ -1,6 +1,49 @@
 // Copyright (c) 2024 Tobias Briones. All rights reserved.
 // This file is part of https://github.com/mathswe/mathswe.com
 
+function Cookies() {
+    return <>
+        <h2>Cookies</h2>
+
+        <p>
+            In optimizing product experience, we utilize cookies
+            for
+            specific functionalities. Cookies are compact text
+            files
+            stored on user devices.
+        </p>
+
+        <blockquote>
+            <p>
+                Cookies are small pieces of text sent to your
+                browser by a website you visit. They help that
+                website remember information about your visit,
+                which
+                can both make it easier to visit the site again
+                and
+                make the site more useful to you.
+            </p>
+
+            <p>
+                Source: How Google Uses Cookies | Google
+            </p>
+        </blockquote>
+
+        <p>
+            You can read the Google cookie policies page
+            to&nbsp;
+            <a
+                href="https://policies.google.com/technologies/cookies"
+                target="_blank"
+                rel="noreferrer"
+            >
+                learn about cookies
+            </a>
+            , and their usage.
+        </p>
+    </>;
+}
+
 function Legal() {
     return <>
         <section className="main">
@@ -11,44 +54,7 @@ function Legal() {
                     </section>
 
                     <section>
-                        <h2>Cookies</h2>
-
-                        <p>
-                            In optimizing product experience, we utilize cookies
-                            for
-                            specific functionalities. Cookies are compact text
-                            files
-                            stored on user devices.
-                        </p>
-
-                        <blockquote>
-                            <p>
-                                Cookies are small pieces of text sent to your
-                                browser by a website you visit. They help that
-                                website remember information about your visit,
-                                which
-                                can both make it easier to visit the site again
-                                and
-                                make the site more useful to you.
-                            </p>
-
-                            <p>
-                                Source: How Google Uses Cookies | Google
-                            </p>
-                        </blockquote>
-
-                        <p>
-                            You can read the Google cookie policies page
-                            to&nbsp;
-                            <a
-                                href="https://policies.google.com/technologies/cookies"
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                learn about cookies
-                            </a>
-                            , and their usage.
-                        </p>
+                        <Cookies></Cookies>
                     </section>
                 </article>
             </main>
