94e832b29f97e88658682fe49ef28843c1235f48
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-02-19 18:40:04 -0600

    Implement UI for component CookieBanner

---
+++ src/ui/legal/CookieBanner.tsx
@@ -0,0 +1,73 @@
+// Copyright (c) 2024 Tobias Briones. All rights reserved.
+// This file is part of https://github.com/mathswe/mathswe.com
+
+import "./CookieBanner.css";
+import { Button, Form } from "react-bootstrap";
+
+interface CookieBannerProps {
+    cookiePolicyLink: string;
+}
+
+function CookieBanner({ cookiePolicyLink }: CookieBannerProps) {
+    return <>
+        <div id="cookieBanner">
+            <div className="content">
+                <h5>Cookies</h5>
+
+                <p>
+                    We use cookies to improve user experience. Choose what
+                    cookies you allow us to use. Learn
+                    more in our <a href={ cookiePolicyLink }>Cookies Policy</a>.
+                </p>
+
+                <p>
+                    Your consent will be valid across all our subdomains.
+
+                    You can always set your consent by clicking
+                    the &quot;Cookie Preference&quot; button at the page
+                    footer.
+                </p>
+            </div>
+
+            <div className="action">
+                <Form>
+                    <Form.Check
+                        id="cookieNecessaryCheck"
+                        label="Strictly necessary"
+                        title="Strictly necessary cookies"
+                        type="checkbox"
+                        inline
+                        checked
+                        disabled
+                    />
+                    <Form.Check
+                        id="cookieAnalyticsCheck"
+                        label="Analytics"
+                        title="Analytics cookies"
+                        type="checkbox"
+                        inline
+                    />
+                    <div className="mt-2 d-flex justify-content-between">
+                        <Button
+                            variant="outline-primary"
+                            className="flex-fill"
+                        >
+                            Customize
+                        </Button>
+                        <Button
+                            variant="outline-primary"
+                            className="flex-fill mx-2 mx-md-4"
+                        >
+                            Save Selection
+                        </Button>
+                        <Button variant="primary" className="flex-fill">
+                            Accept All
+                        </Button>
+                    </div>
+                </Form>
+            </div>
+        </div>
+    </>;
+}
+
+export default CookieBanner;
