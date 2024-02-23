63f060434062b50fe0abef8d736ff28c1118c880
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-02-23 14:19:25 -0600

    Define interface GoogleAnalyticsConfig

--- src/analytics/ga4.ts
+++ src/analytics/ga4.ts
@@ -2,7 +2,18 @@
 // This file is part of https://github.com/mathswe/mathswe.com
 
 import ReactGA from "react-ga4";
+import { CookieConsent } from "@persistence/cookie-consent.ts";
 
-export function initializeGA4(id: string) {
+export interface GoogleAnalyticsConfig {
+    id: string;
+    consent: boolean;
+}
+
+export function newGoogleAnalyticsConfig(cookieConsent: CookieConsent): GoogleAnalyticsConfig | undefined {
+    const id = import.meta.env.VITE_ANALYTICS_GTAG_ID;
+    return id ? { id, consent: cookieConsent.analytics } : undefined;
+}
+
+export function initializeGA4({ id }: GoogleAnalyticsConfig) {
     ReactGA.initialize(id);
 }
