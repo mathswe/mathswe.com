68e5159a55cd0c13b215e839bdbd38c0cb3fa701
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-02-23 20:45:08 -0600

    .

--- src/analytics/ga4.ts
+++ src/analytics/ga4.ts
@@ -30,7 +30,6 @@ export function initializeGA4(
         id,
         analyticsStorageConsent,
     }: GoogleAnalyticsConfig) {
-    const tagId = import.meta.env.VITE_ANALYTICS_GTAG_ID;
     const analyticsConsentValue = analyticsStorageConsent ? "granted" : "denied";
 
     window.dataLayer = window.dataLayer || [];
@@ -48,20 +47,20 @@ export function initializeGA4(
         },
     );
     gtag("js", new Date());
-    gtag("config", tagId ?? "");
+    gtag("config", id ?? "");
 
     if (analyticsStorageConsent) {
         gtag("consent", "update", {
             ad_user_data: "granted",
             ad_personalization: "granted",
             ad_storage: "granted",
-            analytics_storage: "granted",
+            analytics_storage: analyticsStorageConsent,
         });
 
         // Load gtag.js script.
         const gtagScript = document.createElement("script");
         gtagScript.async = true;
-        gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=" + tagId;
+        gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=" + id;
 
         const firstScript = document.getElementsByTagName("script")[0];
         firstScript?.parentNode?.insertBefore(gtagScript, firstScript);
