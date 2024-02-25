7098d784710e027a536ae12c0855713de4332d32
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-02-24 18:49:36 -0600

    .

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -2,33 +2,44 @@
 // This file is part of https://github.com/mathswe/mathswe.com
 
 import Footer from "./Footer.tsx";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import CookieBannerConsent from "@app/legal/CookieBannerConsent.tsx";
+import { useCookies } from "react-cookie";
+import {
+    consentCookieName,
+    loadCookieConsent,
+} from "@persistence/cookie-consent.ts";
+import {
+    initializeGoogleAnalytics,
+    loadGoogleAnalyticsTagId,
+    newGoogleAnalyticsConsent,
+    updateGoogleAnalyticsConsent,
+} from "@analytics/ga4.ts";
 
 interface LayoutProps {
     children: ReactNode;
 }
 
 function Layout({ children }: LayoutProps) {
-    // const [ cookies ] = useCookies([ consentCookieName ]);
+    const [ cookies ] = useCookies([ consentCookieName ]);
 
-    // useEffect(() => {
-    //     const gtagId = loadGoogleAnalyticsTagId();
-    //
-    //     if (gtagId) {
-    //         // initializeGoogleAnalytics(gtagId);
-    //     }
-    // }, []);
-    //
-    // useEffect(() => {
-    //     // const cookieConsent = loadCookieConsent(cookies);
-    //     // const googleConsent = newGoogleAnalyticsConsent(cookieConsent);
-    //     const gtagId = loadGoogleAnalyticsTagId();
-    //
-    //     if (gtagId) {
-    //         // updateGoogleAnalyticsConsent(gtagId, googleConsent);
-    //     }
-    // }, [ cookies ]);
+    useEffect(() => {
+        const gtagId = loadGoogleAnalyticsTagId();
+
+        if (gtagId) {
+            initializeGoogleAnalytics(gtagId);
+        }
+    }, []);
+
+    useEffect(() => {
+        const cookieConsent = loadCookieConsent(cookies);
+        const googleConsent = newGoogleAnalyticsConsent(cookieConsent);
+        const gtagId = loadGoogleAnalyticsTagId();
+
+        if (gtagId) {
+            updateGoogleAnalyticsConsent(gtagId, googleConsent);
+        }
+    }, [ cookies ]);
 
     return <>
         { children }
