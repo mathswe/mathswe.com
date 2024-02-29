43ade73bde6208952f9e2cdedc0eca0e24d8894d
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-02-29 17:47:39 -0600

    Create CookiePreference.tsx

---
+++ src/ui/legal/CookiePreference.tsx
@@ -0,0 +1,8 @@
+// Copyright (c) 2024 Tobias Briones. All rights reserved.
+// This file is part of https://github.com/mathswe/mathswe.com
+
+function CookiePreference() {
+    return <></>;
+}
+
+export default CookiePreference;
