ff2ccc60da37070eefaecef1f255d588c6c18528
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-02-28 22:29:41 -0600

    Remove redundant div in CookieBanner.tsx

--- src/ui/legal/CookieBanner.tsx
+++ src/ui/legal/CookieBanner.tsx
@@ -117,101 +117,99 @@ function CookieAction({ onSave, form }: CookieActionProps) {
     }, [ form ]);
 
     return <>
-        <div className="action">
-            <Form>
-                <div className="d-grid gap-3 gap-md-3">
-                    <div
-                        className="d-flex"
-                        style={ {
-                            gridRowStart: 1,
-                            gridRowEnd: "span 2",
-                            gridColumnStart: 2,
-                            gridColumnEnd: "span 2",
-                        } }
-                    >
-                        <div className="check-col me-3">
-                            <Form.Check
-                                id="cookieNecessaryCheck"
-                                label="Essential"
-                                title="Essential cookies"
-                                type="checkbox"
-                                checked
-                                disabled
-                            />
-                            <CheckAction
-                                name="functional"
-                                state={ functional }
-                                onChange={ setFunctional }
-                            />
-                        </div>
-                        <div className="check-col">
-                            <CheckAction
-                                name="analytics"
-                                state={ analytics }
-                                onChange={ setAnalytics }
-                            />
-                            <CheckAction
-                                name="targeting"
-                                state={ targeting }
-                                onChange={ setTargeting }
-                            />
-                        </div>
-
+        <Form>
+            <div className="d-grid gap-3 gap-md-3">
+                <div
+                    className="d-flex"
+                    style={ {
+                        gridRowStart: 1,
+                        gridRowEnd: "span 2",
+                        gridColumnStart: 2,
+                        gridColumnEnd: "span 2",
+                    } }
+                >
+                    <div className="check-col me-3">
+                        <Form.Check
+                            id="cookieNecessaryCheck"
+                            label="Essential"
+                            title="Essential cookies"
+                            type="checkbox"
+                            checked
+                            disabled
+                        />
+                        <CheckAction
+                            name="functional"
+                            state={ functional }
+                            onChange={ setFunctional }
+                        />
+                    </div>
+                    <div className="check-col">
+                        <CheckAction
+                            name="analytics"
+                            state={ analytics }
+                            onChange={ setAnalytics }
+                        />
+                        <CheckAction
+                            name="targeting"
+                            state={ targeting }
+                            onChange={ setTargeting }
+                        />
                     </div>
 
-                    <Button
-                        variant="primary"
-                        style={ {
-                            gridRowStart: 2,
-                            gridRowEnd: 2,
-                            gridColumnStart: 1,
-                            gridColumnEnd: 1,
-                        } }
-                        onClick={ essentialOnly }
-                    >
-                        Essential Only
-                    </Button>
-
-                    <Button
-                        variant="primary"
-                        style={ {
-                            gridRowStart: 3,
-                            gridRowEnd: 3,
-                            gridColumnStart: 1,
-                            gridColumnEnd: 1,
-                        } }
-                        onClick={ acceptAll }
-                    >
-                        Accept All
-                    </Button>
-
-                    <Button
-                        variant="outline-primary"
-                        style={ {
-                            gridRowStart: 3,
-                            gridRowEnd: 3,
-                            gridColumnStart: 2,
-                            gridColumnEnd: 2,
-                        } }
-                        onClick={ saveSelection }
-                    >
-                        Save Selection
-                    </Button>
-
-                    <Button
-                        variant="outline-primary"
-                        style={ {
-                            gridRowStart: 3,
-                            gridRowEnd: 3,
-                            gridColumnStart: 3,
-                            gridColumnEnd: 3,
-                        } }
-                    >
-                        Customize
-                    </Button>
                 </div>
-            </Form>
-        </div>
+
+                <Button
+                    variant="primary"
+                    style={ {
+                        gridRowStart: 2,
+                        gridRowEnd: 2,
+                        gridColumnStart: 1,
+                        gridColumnEnd: 1,
+                    } }
+                    onClick={ essentialOnly }
+                >
+                    Essential Only
+                </Button>
+
+                <Button
+                    variant="primary"
+                    style={ {
+                        gridRowStart: 3,
+                        gridRowEnd: 3,
+                        gridColumnStart: 1,
+                        gridColumnEnd: 1,
+                    } }
+                    onClick={ acceptAll }
+                >
+                    Accept All
+                </Button>
+
+                <Button
+                    variant="outline-primary"
+                    style={ {
+                        gridRowStart: 3,
+                        gridRowEnd: 3,
+                        gridColumnStart: 2,
+                        gridColumnEnd: 2,
+                    } }
+                    onClick={ saveSelection }
+                >
+                    Save Selection
+                </Button>
+
+                <Button
+                    variant="outline-primary"
+                    style={ {
+                        gridRowStart: 3,
+                        gridRowEnd: 3,
+                        gridColumnStart: 3,
+                        gridColumnEnd: 3,
+                    } }
+                >
+                    Customize
+                </Button>
+            </div>
+        </Form>
     </>;
 }
 
