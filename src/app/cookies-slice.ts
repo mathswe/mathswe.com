731694483922d2e295cb1c58505ccb5efb409135
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-02-29 19:03:46 -0600

    Add global state showingCustomization to cookies-slice.ts

--- src/app/cookies-slice.ts
+++ src/app/cookies-slice.ts
@@ -6,10 +6,12 @@ import { RootState } from "./store.ts";
 
 export interface CookieState {
     showingBanner: boolean;
+    showingCustomization: boolean;
 }
 
 const initialState: CookieState = {
     showingBanner: false,
+    showingCustomization: false,
 };
 
 export const cookieSlice = createSlice({
@@ -22,11 +24,24 @@ export const cookieSlice = createSlice({
         hideCookieBanner: state => {
             state.showingBanner = false;
         },
+        showCookieCustomization: state => {
+            state.showingCustomization = true;
+        },
+        hideCookieCustomization: state => {
+            state.showingCustomization = false;
+        },
     },
 });
 
-export const { showCookieBanner, hideCookieBanner } = cookieSlice.actions;
+export const {
+    showCookieBanner,
+    hideCookieBanner,
+    showCookieCustomization,
+    hideCookieCustomization,
+} = cookieSlice.actions;
 
 export const selectShowingBanner = (state: RootState) => state.cookies.showingBanner;
+
+export const selectShowingCustomization = (state: RootState) => state.cookies.showingCustomization;
 
 export default cookieSlice.reducer;
