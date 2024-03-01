c8b0d13a448a7b616a2fe086991d4b181fb3b10d
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-02-29 21:00:42 -0600

    Implement basic component CookieCustomization from CookieBanner

--- src/app/legal/AppCookieCustomization.tsx
+++ src/app/legal/AppCookieCustomization.tsx
@@ -1,11 +1,81 @@
 // Copyright (c) 2024 Tobias Briones. All rights reserved.
 // This file is part of https://github.com/mathswe/mathswe.com
 
+import { CookiePref, defPref } from "@ui/legal/cookie-pref.ts";
+import { useAppDispatch, useAppSelector } from "@app/hooks.ts";
+import {
+    hideCookieCustomization,
+    selectShowingCustomization,
+    showCookieBanner,
+} from "@app/cookies-slice.ts";
+import { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
+import {
+    applyConsent,
+    consentCookieName,
+    CookieConsent,
+    loadCookieConsent,
+} from "@persistence/cookie-consent.ts";
 import CookieCustomization from "@ui/legal/CookieCustomization.tsx";
 
+const cookiePolicyLink = "/legal/cookie-privacy";
+
+function newCookieConsent(
+    {
+        functional,
+        analytics,
+        targeting,
+    }: CookiePref,
+): CookieConsent {
+    return {
+        necessary: true,
+        functional: functional ?? false,
+        analytics: analytics ?? false,
+        targeting: targeting ?? false,
+    };
+}
+
 function AppCookieBanner() {
+    const showingCustomization = useAppSelector(selectShowingCustomization);
+    const dispatch = useAppDispatch();
+
+    const closeCustomization = () => { dispatch(hideCookieCustomization()); };
+
+    const [ cookies, setCookie ] = useCookies([ consentCookieName ]);
+
+    const [ pref, setPref ] = useState(defPref);
+
+    const [ domainName, setDomainName ] = useState("");
+
+    const save = (pref: CookiePref) => {
+        const consent = newCookieConsent(pref);
+        const { cookieName, consentSer, options } = applyConsent(consent);
+
+        setCookie(cookieName, consentSer, options);
+        closeCustomization();
+    };
+
+    useEffect(() => {
+        const { functional, analytics, targeting } = loadCookieConsent(cookies);
+
+        setPref({ functional, analytics, targeting });
+
+        if (!cookies[consentCookieName]) {
+            dispatch(showCookieBanner());
+        }
+    }, [ cookies, dispatch ]);
+
+    useEffect(() => setDomainName(import.meta.env.VITE_DOMAIN_NAME ?? ""), []);
+
     return <>
-        <CookieCustomization />
+        <CookieCustomization
+            domainName={ domainName }
+            cookiePolicyLink={ cookiePolicyLink }
+            show={ showingCustomization }
+            initialForm={ pref }
+            onSave={ save }
+            onClose={ closeCustomization }
+        />
     </>;
 }
 
--- src/ui/legal/CookieCustomization.tsx
+++ src/ui/legal/CookieCustomization.tsx
@@ -1,8 +1,222 @@
 // Copyright (c) 2024 Tobias Briones. All rights reserved.
 // This file is part of https://github.com/mathswe/mathswe.com
 
-function CookieCustomization() {
-    return <></>;
+import React, { useCallback, useEffect, useReducer, useState } from "react";
+import { usePrevious } from "@app/hooks.ts";
+import "./CookieCustomization.css";
+import { Button, Form } from "react-bootstrap";
+import CloseIcon from "@ui/CloseIcon.tsx";
+import { acceptAllPref, CookiePref, defPref } from "./cookie-pref.ts";
+import CookieContent from "@ui/legal/CookieContent.tsx";
+
+interface CheckActionProps {
+    name: string;
+    onChange: (check: boolean) => void;
+    state?: boolean;
+}
+
+function CheckAction({ name, onChange, state }: CheckActionProps) {
+    return state !== undefined && <>
+        <Form.Check
+            id={ `${ name }CookieCheck` }
+            label={ name.toUpperCase() }
+            title={ `${ name } cookies` }
+            type="checkbox"
+            onChange={ e => onChange(e.target.checked) }
+            checked={ state }
+        />
+    </>;
+}
+
+interface CookieActionProps {
+    onSave: (pref: CookiePref) => void;
+    onCancel: () => void;
+    form: CookiePref;
+}
+
+function CookieAction({ onSave, onCancel, form }: CookieActionProps) {
+    const [ functional, setFunctional ] = useState<boolean | undefined>();
+    const [ analytics, setAnalytics ] = useState<boolean | undefined>();
+    const [ targeting, setTargeting ] = useState<boolean | undefined>();
+
+    const essentialOnly = () => { onSave(defPref); };
+
+    const acceptAll = () => { onSave(acceptAllPref); };
+
+    const saveSelection = () => {
+        onSave({
+            functional,
+            analytics,
+            targeting,
+        });
+    };
+
+    useEffect(() => {
+        setFunctional(form.functional);
+        setAnalytics(form.analytics);
+        setTargeting(form.targeting);
+    }, [ form ]);
+
+    return <>
+        <Form>
+            <div
+                className="d-flex mb-3"
+            >
+                <div className="check-col me-3">
+                    <Form.Check
+                        id="cookieNecessaryCheck"
+                        label="Essential"
+                        title="Essential cookies"
+                        type="checkbox"
+                        checked
+                        disabled
+                    />
+                    <CheckAction
+                        name="functional"
+                        state={ functional }
+                        onChange={ setFunctional }
+                    />
+                </div>
+                <div className="check-col">
+                    <CheckAction
+                        name="analytics"
+                        state={ analytics }
+                        onChange={ setAnalytics }
+                    />
+                    <CheckAction
+                        name="targeting"
+                        state={ targeting }
+                        onChange={ setTargeting }
+                    />
+                </div>
+
+            </div>
+
+            <div className="d-grid gap-3 gap-md-3">
+                <Button
+                    variant="primary"
+                    style={ {
+                        gridRowStart: 1,
+                        gridRowEnd: 1,
+                        gridColumnStart: 1,
+                        gridColumnEnd: 1,
+                    } }
+                    onClick={ essentialOnly }
+                >
+                    Essential Only
+                </Button>
+
+                <Button
+                    variant="primary"
+                    style={ {
+                        gridRowStart: 2,
+                        gridRowEnd: 2,
+                        gridColumnStart: 1,
+                        gridColumnEnd: 1,
+                    } }
+                    onClick={ acceptAll }
+                >
+                    Accept All
+                </Button>
+
+                <Button
+                    variant="outline-primary"
+                    style={ {
+                        gridRowStart: 2,
+                        gridRowEnd: 2,
+                        gridColumnStart: 2,
+                        gridColumnEnd: 2,
+                    } }
+                    onClick={ saveSelection }
+                >
+                    Save Selection
+                </Button>
+
+                <Button
+                    variant="outline-primary"
+                    style={ {
+                        gridRowStart: 2,
+                        gridRowEnd: 2,
+                        gridColumnStart: 3,
+                        gridColumnEnd: 3,
+                    } }
+                    onClick={ onCancel }
+                >
+                    Cancel
+                </Button>
+            </div>
+        </Form>
+    </>;
+}
+
+interface CookiePreferenceProps {
+    domainName: string;
+    cookiePolicyLink: string;
+    initialForm: CookiePref;
+    show: boolean;
+    onSave: (pref: CookiePref) => void;
+    onClose: () => void;
+}
+
+function CookieCustomization(
+    {
+        domainName,
+        cookiePolicyLink,
+        initialForm,
+        show,
+        onSave,
+        onClose,
+    }: CookiePreferenceProps,
+) {
+    const formReducer = (_: CookiePref, newForm: CookiePref) => newForm;
+    const [ form, setForm ] = useReducer(formReducer, initialForm);
+    const [ className, setClassName ] = useState("");
+    const [ open, setOpen ] = useState(false);
+    const prevOpen = usePrevious(open);
+
+    type TransitionHandler = React.TransitionEventHandler<HTMLDivElement>;
+
+    const handleTransitionEnd: TransitionHandler = e => {
+        if (!show && e.propertyName === "transform") {
+            setOpen(false);
+        }
+    };
+
+    const onTransitionEnd = useCallback(handleTransitionEnd, [ show ]);
+
+    useEffect(() => {
+        if (show) {
+            setOpen(true);
+        }
+        setClassName(show ? "show" : "");
+    }, [ show ]);
+
+    useEffect(() => {
+        if (open && !prevOpen) {
+            setForm(initialForm);
+        }
+    }, [ open, prevOpen, initialForm ]);
+
+    return (open || show) && <>
+        <div
+            id="cookieCustomization"
+            className={ className }
+            onTransitionEnd={ onTransitionEnd }
+        >
+            <CookieContent
+                domainName={ domainName }
+                cookiePolicyLink={ cookiePolicyLink }
+            />
+
+            <CookieAction
+                onSave={ onSave }
+                onCancel={ onClose }
+                form={ form }
+            />
+
+            <CloseIcon onClose={ onClose } />
+        </div>
+    </>;
 }
 
 export default CookieCustomization;
