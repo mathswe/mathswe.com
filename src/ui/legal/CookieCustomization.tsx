363c12006ddfc21b25fff87fbf36966b2b40ec21
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-04-02 17:51:53 -0600

    Add description to category items in component CookieCustomization

--- src/ui/legal/CookieCustomization.tsx
+++ src/ui/legal/CookieCustomization.tsx
@@ -214,6 +214,15 @@ function CookieAction({ onSave, onCancel, form }: CookieActionProps) {
                                 />
                             </div>
 
+                            <p>
+                                These are necessary for the website or web app
+                                to function properly and do not require user
+                                consent. They typically store session
+                                information or user preferences. The website
+                                cannot be used properly without these strictly
+                                necessary cookies.
+                            </p>
+
                             <CookieCategoryDetails rows={ cookies } />
                         </div>
                     </Item>
@@ -229,6 +238,20 @@ function CookieAction({ onSave, onCancel, form }: CookieActionProps) {
                                     onChange={ setFunctional }
                                 />
                             </div>
+
+                            <p>
+                                These enhance the website or web app performance
+                                as certain functions may not be available
+                                without them. They allow users to remember their
+                                preferences and settings, provide a personalized
+                                user experience, are anonymous, be first-party
+                                or set by third-party service providers, and do
+                                not track browsing activity across other
+                                websites. For example, cookies that remember
+                                user location, chosen language, or other
+                                settings, a live web chat platform, and optional
+                                security parameters like a single sign-on (SSO).
+                            </p>
 
                             <CookieCategoryDetails rows={ cookies } />
                         </div>
@@ -246,6 +269,15 @@ function CookieAction({ onSave, onCancel, form }: CookieActionProps) {
                                 />
                             </div>
 
+                            <p>
+                                These cookies collect data on how users interact
+                                with the website or web app, including metrics
+                                like page views, bounce rates, and traffic
+                                sources. They cannot be used to directly
+                                identify a certain visitor. They help website
+                                owners understand and improve site performance.
+                            </p>
+
                             <CookieCategoryDetails rows={ cookies } />
                         </div>
                     </Item>
@@ -261,6 +293,19 @@ function CookieAction({ onSave, onCancel, form }: CookieActionProps) {
                                     onChange={ setTargeting }
                                 />
                             </div>
+
+                            <p>
+                                These are used to identify visitors between
+                                different websites and may be used by companies
+                                to build a profile of visitor interests or show
+                                relevant ads on other websites, and are usually
+                                third-party. They are used on a limited basis,
+                                and we do not use them to serve third-party ads
+                                on our websites or web apps. For example,
+                                cookies installed by YouTube in videos embedded
+                                into our site to track their views and user
+                                preferences.
+                            </p>
 
                             <CookieCategoryDetails rows={ cookies } />
                         </div>
