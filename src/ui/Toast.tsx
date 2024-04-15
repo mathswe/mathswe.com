1822c30f2dc3288a12cd738f424bac16bc4b878c
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-04-15 12:36:27 -0600

    Add component NotificationToast to lib ui
    It adds a new component to the UI library that can show short notification messages.

---
+++ src/ui/Toast.tsx
@@ -0,0 +1,56 @@
+// Copyright (c) 2024 Tobias Briones. All rights reserved.
+// This file is part of https://github.com/mathswe/mathswe.com
+
+import { useEffect } from "react";
+import Toast from "react-bootstrap/Toast";
+import "./Toast.css";
+
+export type ToastDuration = 2_000 | 5_000;
+export const SMALL_DURATION: ToastDuration = 2000;
+export const LARGE_DURATION: ToastDuration = 5000;
+
+interface NotificationToastProps {
+    headerTitle: string;
+    body: string;
+    show: boolean;
+    onClose: () => void;
+    smallTitle?: string;
+    duration?: ToastDuration;
+}
+
+function NotificationToast(
+    {
+        headerTitle,
+        body,
+        show,
+        onClose,
+        smallTitle,
+        duration,
+    }: NotificationToastProps,
+) {
+    useEffect(() => {
+        if (show) {
+            const tid = setTimeout(onClose, duration ?? SMALL_DURATION);
+
+            return () => clearTimeout(tid);
+        }
+    }, [ duration, onClose, show ]);
+
+    return (
+        <Toast show={ show } onClose={ onClose }>
+            <Toast.Header>
+                <img
+                    src="/public/mathswe.svg"
+                    width="24px"
+                    className="rounded me-2"
+                    alt="Notification Icon"
+                />
+                <strong className="me-auto">{ headerTitle }</strong>
+                <small>{ smallTitle ?? "" }</small>
+            </Toast.Header>
+            <Toast.Body>{ body }</Toast.Body>
+        </Toast>
+    );
+}
+
+export default NotificationToast;
