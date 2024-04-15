63569bd5fbcff592affbd8a6237d597c991d9a16
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-04-15 12:37:35 -0600

    Integrate component NotificationToast into app
    It integrates the "NotificationToast" component from the UI library to allow the app to show brief update messages.

---
+++ src/app/AppNotificationToast.tsx
@@ -0,0 +1,30 @@
+// Copyright (c) 2024 Tobias Briones. All rights reserved.
+// This file is part of https://github.com/mathswe/mathswe.com
+
+import NotificationToast from "@ui/Toast.tsx";
+import { useAppDispatch, useAppSelector } from "@app/hooks.ts";
+import {
+    hideNotificationToast,
+    selectNotificationToast,
+} from "@app/toast-slice.ts";
+
+function AppNotificationToast() {
+    const dispatch = useAppDispatch();
+    const { headerTitle, body, show, smallTitle, duration }
+        = useAppSelector(selectNotificationToast);
+
+    const onClose = () => dispatch(hideNotificationToast());
+
+    return <>
+        <NotificationToast
+            headerTitle={ headerTitle }
+            body={ body }
+            show={ show }
+            onClose={ onClose }
+            smallTitle={ smallTitle }
+            duration={ duration }
+        />
+    </>;
+}
+
+export default AppNotificationToast;
