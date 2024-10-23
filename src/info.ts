e4d8c05b2c2a62a4f94d71b92f325a1b67d1011e
Tobias Briones <tbriones@mathswe.com>
2024-10-23 10:29:53 -0600

    Set project version to 0.1.0-dev

--- src/info.ts
+++ src/info.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2024 Tobias Briones. All rights reserved.
 // This file is part of https://github.com/mathswe/mathswe.com
 
-export const APP_SEMVER = "0.1.0";
+export const APP_SEMVER = "0.1.0-dev";
 export const APP_NAME = "MathSwe Com";
 export const APP_VERSION = `v${ APP_SEMVER }`;
 export const APP_VERSION_LABEL = `${ APP_NAME } ${ APP_VERSION }`;
